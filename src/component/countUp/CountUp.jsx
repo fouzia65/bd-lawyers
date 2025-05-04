b3901129e2244eb7dbd7469b1536f77dd4f220d3
fouzia65 <fouziarahman1234567890@gmail.com>
2025-05-04 16:04:03 +0600

    second commit

--- src/component/countUp/CountUp.jsx
+++ src/component/countUp/CountUp.jsx
@@ -1,61 +1,44 @@
+// src/components/CountUp1.jsx
+
 import React from 'react';
-import img1 from '../../assets/success-doctor.png'
-import img2 from '../../assets/success-patients.png'
-import img3 from '../../assets/success-review.png'
-import img4 from '../../assets/success-staffs.png'
+import img1 from '../../assets/success-doctor.png';
+import img2 from '../../assets/success-patients.png';
+import img3 from '../../assets/success-review.png';
+import img4 from '../../assets/success-staffs.png';
 import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
+
+
+const StatBox = ({ img, end, label }) => {
+    const { ref, inView } = useInView({
+        triggerOnce: true,  
+    });
+
+    return (
+        <div ref={ref} className='bg-gray-300 hover:bg-white py-10 px-8 m-4 rounded-3xl text-center shadow-md transition duration-300'>
+            <img src={img} alt={label} className="mx-auto mb-4 w-24 h-24 object-contain" />
+            {inView && (
+                <CountUp start={0} end={end} duration={2.75}>
+                    {({ countUpRef }) => (
+                        <div>
+                            <span className='text-5xl font-extrabold text-indigo-700' ref={countUpRef} />+
+                        </div>
+                    )}
+                </CountUp>
+            )}
+            <p className='mt-2 text-lg font-semibold'>{label}</p>
+        </div>
+    );
+};
+
 
 const CountUp1 = () => {
     return (
-        <div className='flex text-black '>
-            <div className='bg-gray-300 hover:bg-white py-15 px-25 m-4 rounded-3xl'>
-                <img src={img1} alt="" />
-                <CountUp start={0} end={199} duration={2.75} className='text-2xl'>
-                    {({ countUpRef}) => (
-                        <div>
-                            <span className='text-5xl font-extrabold' ref={countUpRef} />+
-                            
-                        </div>
-                    )}
-                </CountUp>
-                <p>total lawyer</p>
-            </div>
-            <div className='bg-gray-300 hover:bg-white py-15 px-25 m-4 rounded-3xl'>
-                <img src={img3} alt="" />
-                <CountUp start={0} end={199} duration={2.75} className='text-2xl'>
-                    {({ countUpRef}) => (
-                        <div>
-                            <span className='text-5xl font-extrabold' ref={countUpRef} />+
-                            
-                        </div>
-                    )}
-                </CountUp>
-                <p>Total review</p>
-            </div>
-            <div className='bg-gray-300 hover:bg-white py-15 px-25 m-4 rounded-3xl'>
-                <img src={img2} alt="" />
-                <CountUp start={0} end={1900} duration={2.75} className='text-2xl'>
-                    {({ countUpRef}) => (
-                        <div>
-                            <span className='text-5xl font-extrabold' ref={countUpRef} />+
-                            
-                        </div>
-                    )}
-                </CountUp>
-                <p>cases initiated</p>
-            </div>
-            <div className='bg-gray-300 hover:bg-white py-15 px-25 m-4 rounded-3xl'>
-                <img src={img4} alt="" />
-                <CountUp start={0} end={300} duration={2.75} className='text-2xl'>
-                    {({ countUpRef}) => (
-                        <div>
-                            <span className='text-5xl font-extrabold' ref={countUpRef} />+
-                            
-                        </div>
-                    )}
-                </CountUp>
-                <p>total staffs</p>
-            </div>
+        <div className='flex flex-wrap justify-center items-center text-black py-10 bg-gray-100'>
+            <StatBox img={img1} end={199} label="Total Lawyers" />
+            <StatBox img={img3} end={199} label="Total Reviews" />
+            <StatBox img={img2} end={1900} label="Cases Initiated" />
+            <StatBox img={img4} end={300} label="Total Staffs" />
         </div>
     );
 };
